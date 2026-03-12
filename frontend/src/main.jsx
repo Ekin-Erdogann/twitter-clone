dda59ccd68b66b073f0ba73fe61e2806c7a55d18
Ekin-Erdogann <ekin.erdogan@outlook.com>
2026-03-12 23:17:11 +0300

    bug fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -15,9 +15,9 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClient.Provider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
     <App />
-      </QueryClient.Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 )
